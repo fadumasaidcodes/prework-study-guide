7cc3ef1d7af0622d18717a32486d60c4b8783203
fadumasaidcodes <fadumasaid2022@gmail.com>
2022-11-28 10:02:09 +0000

    I have added a change

--- assets/script.js
+++ assets/script.js
@@ -12,10 +12,10 @@
 //   console.log('Please try again!');
 // }
 
-// var shapes = ["triangle", "square", "pentagon", "circle"];
-// for(var x = 0; x < shapes.length; x++) {
-//  console.log(shapes[x]);
-// }
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 for(var x = 0; x < topics.length; x++) {
